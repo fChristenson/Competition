8a497012e58d9277b0d01012ab115051004d90bf
fidde <fredrik.christenson@gmail.com>
2014-04-17 18:49:17 +0200

    found out about more java 8 features

--- src/main/java/se/fidde/codemintcompetition/Main.java
+++ src/main/java/se/fidde/codemintcompetition/Main.java
@@ -1,132 +1,70 @@
 package se.fidde.codemintcompetition;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.ForkJoinPool;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
+import java.net.URISyntaxException;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Spliterator;
+import java.util.function.BiPredicate;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static String errorOutputFile;
+    private static String outputFile;
+    private static String folderToScan;
+    private static FileVisitOption options;
+    private static BiPredicate<Path, BasicFileAttributes> matcher;
+    private static Path start;
+
+    public static void main(String[] args) throws IOException {
         validateInputArgs(args);
-        File folderToScan = getFolderToScan(args);
+        folderToScan = args[0];
+        outputFile = args[1];
+        if (args.length == 3)
+            errorOutputFile = args[2];
 
         System.out.println("Processing...");
 
-        process(folderToScan, args[1]);
+        try {
+            process(folderToScan);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
 
         System.out.println("Done.");
     }
 
-    private static void process(File folderToScan, String outputFile) {
-        List<File> folders = getSubFolders(folderToScan);
-        ForkJoinPool pool = new ForkJoinPool();
+    private static void process(String folderToScan) throws IOException,
+            URISyntaxException {
+        File file = new File(folderToScan);
 
-        folders.forEach(file -> {
-            File[] listFiles = file.listFiles();
-            List<File> filterList = filterFiles(listFiles);
+        validateFolderToScan(file);
 
-            GzipHandler gzipHandler = new GzipHandler(filterList);
-            List<ProcessedPost> dataFromFolder = pool.invoke(gzipHandler);
+        start = file.toPath();
+        matcher = getMatcher();
+        options = FileVisitOption.FOLLOW_LINKS;
 
-            writeDataToFile(dataFromFolder, outputFile);
-        });
+        Stream<Path> find = Files.find(start, 2, matcher, options).parallel();
+        Spliterator<Path> spliterator = find.spliterator().trySplit();
+
+        // TODO: get streams to files and get data
+
     }
 
-    private static void filterData(List<ProcessedPost> dataFromFolder) {
-        Predicate<? super ProcessedPost> filter = post -> {
-            return post.getRows() == null || post.getRows().size() == 0;
+    private static BiPredicate<Path, BasicFileAttributes> getMatcher() {
+        return (path, basicFileAttr) -> {
+
+            return Pattern.matches(".+\\.gz", path.toString());
         };
-        dataFromFolder.removeIf(filter);
     }
 
-    private static File getFolderToScan(String[] args) {
-        File folderToScan = new File(args[0]);
-        validateFolderToScan(folderToScan);
-        return folderToScan;
-    }
-
-    private static List<File> getSubFolders(File folderToScan) {
-        File[] listFiles = folderToScan.listFiles();
-        List<File> folders = filterFolders(listFiles);
-
-        return folders;
-    }
-
-    private static List<File> filterFolders(File[] listFiles) {
-        List<File> asList = Arrays.asList(listFiles);
-        Predicate<? super File> predicate = file -> {
-            if (file.isDirectory())
-                return true;
-
-            return false;
-        };
-        List<File> result = asList.stream().filter(predicate)
-                .collect(Collectors.toList());
-
-        return result;
-    }
-
-    private static void writeDataToFile(List<ProcessedPost> dataFromFolder,
-            String fileName) {
-
-        filterData(dataFromFolder);
-        if (dataFromFolder.size() == 0)
-            return;
-
-        File file = new File(fileName);
-        FileWriter fileWriter = null;
-        BufferedWriter bufferedWriter = null;
-
-        try {
-            fileWriter = new FileWriter(file, true);
-            bufferedWriter = new BufferedWriter(fileWriter);
-            String valueString = getValues(dataFromFolder);
-
-            bufferedWriter.write(valueString);
-            bufferedWriter.newLine();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-
-        } finally {
-            if (bufferedWriter != null)
-                try {
-                    bufferedWriter.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-        }
-    }
-
-    private static String getValues(List<ProcessedPost> dataFromFolder) {
-        String year = dataFromFolder.get(0).getRows().get(0).getYear();
-        int max = dataFromFolder.parallelStream()
-                .mapToInt(post -> post.getMaxTemp()).sum();
-
-        int avg = dataFromFolder.parallelStream()
-                .mapToInt(post -> post.getAvgTemp()).sum();
-
-        int min = dataFromFolder.parallelStream()
-                .mapToInt(post -> post.getMinTemp()).sum();
-
-        int length = dataFromFolder.size();
-        max = max / length;
-        avg = avg / length;
-        min = min / length;
-
-        String format = String.format("%s;%s;%s;%s", year, max, avg, min);
-        return format;
-    }
-
-    private static void validateFolderToScan(File folderToScan) {
-        if (!folderToScan.exists() || !folderToScan.isDirectory()
-                || !folderToScan.canRead()) {
+    private static void validateFolderToScan(File file) {
+        if (!file.exists() || !file.isDirectory() || !file.canRead()) {
             System.out.println("Provided folder is not valid");
             System.out
                     .println("Please make sure that the path is correct and you have read/write access");
@@ -143,18 +81,4 @@ public class Main {
         }
     }
 
-    private static List<File> filterFiles(File[] listFiles) {
-        List<File> asList = Arrays.asList(listFiles);
-        Predicate<? super File> predicate = file -> {
-            if (file.isDirectory()
-                    || !file.getName().matches("\\d{6}-\\d{5}-\\d{4}.gz"))
-                return false;
-
-            return true;
-        };
-        List<File> collect = asList.stream().filter(predicate)
-                .collect(Collectors.toList());
-
-        return collect;
-    }
 }
