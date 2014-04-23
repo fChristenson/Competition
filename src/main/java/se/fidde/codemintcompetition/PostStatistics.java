3b38eece9f541853e3d7079efbe24a88c8f05522
fidde <fredrik.christenson@gmail.com>
2014-04-23 05:29:37 +0200

    added more stuff

---
+++ src/main/java/se/fidde/codemintcompetition/PostStatistics.java
@@ -0,0 +1,36 @@
+package se.fidde.codemintcompetition;
+
+import java.util.IntSummaryStatistics;
+
+public class PostStatistics {
+    private String year;
+    private IntSummaryStatistics statistics;
+
+    public PostStatistics(String year, IntSummaryStatistics statistics) {
+        this.setYear(year);
+        this.statistics = statistics;
+    }
+
+    public IntSummaryStatistics getStatistics() {
+        return statistics;
+    }
+
+    public void setStatistics(IntSummaryStatistics statistics) {
+        this.statistics = statistics;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "PostStatistics [year=" + year + ", statistics=" + statistics
+                + "]";
+    }
+
+}
